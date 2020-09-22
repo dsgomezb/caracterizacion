631cfb3d710cbf0a2687e098be14712356dfb02c
PRUEBAS TI <pruebasti@UMANIZALES.LOCAL>
2020-09-21 22:34:51 -0500

    api persona finca

---
+++ back/src/routes/encuesta.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const { isNotLoggedIn } = require('../lib/auth');
+const pool = require('../database');
+const helpers = require('../lib/helpers');
+
+
+
+//Api para obtener las encuestas activas
+router.post('/api/get_encuesta', async (req, res) => {
+    let encuesta = await pool.query("SELECT id, nombre FROM encuesta where activo is true order by nombre");
+     if (encuesta.rows.length > 0) {
+        data = {
+            "code": "0",
+            "data": encuesta,
+
+        };
+    } else {
+        data = {
+            "code": "1",
+            "error": "No existen encuestas activas"
+
+        };
+    }
+    res.status(200).json(data);
+});
+
+
+
+module.exports = router;
